e50e61cb77bd84a5c6f5da11a1ee3756364bc9e1
Sherin Olivia <SOChronicle@gmail.com>
2023-10-24 21:51:05 +0700

    chore: edited port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,13 @@ import insertAdmin from './config/adminConfig';
 import router from './router/mainRouter';
 import appMiddleware from './middleware';
 // import * as functions from 'firebase-functions';
-import http, { Server } from 'http'
+// import http, { Server } from 'http'
 
 const app: Express = express()
-const server: Server = http.createServer(app)
-let PORT: number;
+const port = process.env.PORT || 8080;
+
+// const server: Server = http.createServer(app)
+// let PORT: number;
 
 // middleware
 appMiddleware(app)
@@ -38,15 +40,18 @@ insertAdmin();
 // router
 app.use(router)
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
-server.listen(0, () => {
-    const address = server.address();
-    if(address && typeof address !== 'string'){
-        PORT = address.port || 5555;
-        console.log(`Server is running on port:${PORT}`)
-    } else {
-        console.error("Server address is not available.")
-    }
-})
+// server.listen(0, () => {
+//     const address = server.address();
+//     if(address && typeof address !== 'string'){
+//         PORT = address.port || 5555;
+//         console.log(`Server is running on port:${PORT}`)
+//     } else {
+//         console.error("Server address is not available.")
+//     }
+// })
 
 // export const week_17_sherinolivia = functions.https.onRequest(app)
